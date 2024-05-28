8e7de85a0be7678766141acbe4b5ce8409bc433a
filizzolalucca <40776137+filizzolalucca@users.noreply.github.com>
2024-05-28 01:18:21 -0300

    criando tela principal(home do app)

---
+++ quiz_teste/quiz/lib/modules/change_user/view/change_user_data_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class ChangeUserPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Tela de alterar dados',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
---
+++ quiz_teste/quiz/lib/modules/select_subjects/view/subjects_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class SchoolSubjectScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Quiz - em breve',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
